6ff4682c8b74fbc5180cccaa6214b2b22c86c6d9
User <User@WIN-O89KV8P9K8M>
2014-02-05 23:46:56 +0500

    Первые файлы

--- protected/views/site/voting.php
+++ protected/views/site/voting.php
@@ -1,15 +1,17 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle = Yii::app()->name;
 ?>
 
-<h1><?=$title?></h1>
+<h1><?= $title ?></h1>
 
 <?php
-
-foreach ($row as $key => $val) {    
-    echo "<p>$val[name]</p>";
+if (!empty($row)) {
+    foreach ($row as $key => $val) {
+        echo "<p>$val[name]</p>";
+    }
+} else {
+    echo 'Вариантов ответа для данного голосования нет.';
 }
-
 ?>
